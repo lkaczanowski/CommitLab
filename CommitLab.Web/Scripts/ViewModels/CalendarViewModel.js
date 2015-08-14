b6a0db001db94a4b4c80f570f60e1110c24c0e4f
azuraw10 <azuraw10@gmail.com>
2015-08-14 14:25:11 +0200

    grouped commits by repository names

--- CommitLab.Web/Scripts/ViewModels/CalendarViewModel.js
+++ CommitLab.Web/Scripts/ViewModels/CalendarViewModel.js
@@ -4,7 +4,9 @@ var _data; //all data, commits, dates, branches, repositories ... not need proba
 var uniqueDates = []
 var unixtimestamp = [] //sorted dates unix time format
 
-var repoDateMap = [];
+var repoDateMap = []
+
+
 
 function getDate(value)  //FORMAT: MONTH DD, YYYY
 {
@@ -84,16 +86,19 @@ var CalendarViewModel = (function () {
 
         for (var i = 0; i < data.length; i++) {
           var d = getUnixTimestamp(data[i]);
-          repoDateMap.push({ repo: data[i].repositoryName, time: d });
+          repoDateMap.push({ repo: data[i].repositoryName, time: d, branch: data[i].branchName });
         }
         repoDateMap.sort(compare);
         for (var i = 0; i < repoDateMap.length; i++) {
           repoDateMap[i].time = new Date(repoDateMap[i].time * 1000);
         }
-        updateCommits();
-        unixtimestamp.sort();
 
-        uniqueDates.push(repoDateMap[0].time);
+        updateCommits();
+
+        unixtimestamp.sort();
+        if (repoDateMap.length > 0) {
+          uniqueDates.push(repoDateMap[0].time);
+        }
         for (var i = 1; i < repoDateMap.length; i++) {
           if (repoDateMap[i - 1].time.getFullYear() === repoDateMap[i].time.getFullYear()
             && repoDateMap[i - 1].time.getMonth() === repoDateMap[i].time.getMonth()
@@ -111,88 +116,96 @@ var CalendarViewModel = (function () {
         _this.periodLastYear(getPeriodString(yearAgo, now));
         _this.commitsLastYear(data.length + " total");
 
-        var dat = new Date();
-        dat.setDate(dat.getDate() - 1);
-        var from = new Date();
-        //from.setDate(from.getDate() - 1);
-        var to = new Date();
-        to.setDate(to.getDate() - 1);
-        var streak = 0;
+        if (data.length > 0) {
+          var dat = new Date();
+          dat.setDate(dat.getDate() - 1);
+          var from = new Date();
+          //from.setDate(from.getDate() - 1);
+          var to = new Date();
+          to.setDate(to.getDate() - 1);
+          var streak = 0;
 
 
-        for (var i = uniqueDates.length - 1; i >= 0; i--) {
-          if (dat.getFullYear() == uniqueDates[i].getFullYear()
-            && dat.getMonth() == uniqueDates[i].getMonth()
-            && dat.getDay() == uniqueDates[i].getDay()) {
-            streak++;
-            dat.setDate(dat.getDate() - 1);
-            from.setDate(from.getDate() - 1);
-          } else {
-            break;
+          for (var i = uniqueDates.length - 1; i >= 0; i--) {
+            if (dat.getFullYear() == uniqueDates[i].getFullYear()
+              && dat.getMonth() == uniqueDates[i].getMonth()
+              && dat.getDay() == uniqueDates[i].getDay()) {
+              streak++;
+              dat.setDate(dat.getDate() - 1);
+              from.setDate(from.getDate() - 1);
+            } else {
+              break;
+            }
           }
-        }
 
-        if (streak === 0) {
-          var d = uniqueDates[uniqueDates.length - 1];
-          var now = new Date();
-          var diff = Math.floor((now - d) / 86400000);
-          _this.currentStreak("0 days");
-          _this.currentStreakPeriod("Last contributed " + diff + " days ago");
+          if (streak === 0) {
+            var d = uniqueDates[uniqueDates.length - 1];
+            var now = new Date();
+            var diff = Math.floor((now - d) / 86400000);
+            _this.currentStreak("0 days");
+            _this.currentStreakPeriod("Last contributed " + diff + " days ago");
+          } else {
+            _this.currentStreak(streak + " days");
+            _this.currentStreakPeriod(getPeriodString(from, to));
+          }
+
+
+
+          var dat = new Date();
+          dat.setDate(dat.getDate() - 1);
+          var from = new Date();
+          from.setDate(from.getDate() - 1);
+          var to = new Date();
+          to.setDate(to.getDate() - 1);
+          var fromBest = new Date();
+          fromBest.setDate(fromBest.getDate() - 1);
+          var toBest = new Date();
+          toBest.setDate(toBest.getDate() - 1);
+
+          var streak = 0;
+          var bestStreak = 0;
+          var isToSet = 0;
+
+          for (var i = uniqueDates.length - 1; i >= 0; i--) {
+            if (dat.getFullYear() == uniqueDates[i].getFullYear()
+              && dat.getMonth() == uniqueDates[i].getMonth()
+              && dat.getDay() == uniqueDates[i].getDay()) {
+              streak++;
+              if (isToSet === 0) {
+                to.setDate(dat.getDate());
+                to.setMonth(dat.getMonth());
+                to.setFullYear(dat.getFullYear());
+                isToSet = 1;
+              }
+              dat.setDate(dat.getDate() - 1);
+            } else {
+              from.setDate(dat.getDate() + 1);
+              from.setMonth(dat.getMonth());
+              from.setFullYear(dat.getFullYear());
+
+              if (streak > bestStreak) {
+                bestStreak = streak;
+                fromBest.setDate(from.getDate());
+                fromBest.setMonth(from.getMonth());
+                fromBest.setFullYear(from.getFullYear());
+                toBest.setDate(to.getDate());
+                toBest.setMonth(to.getMonth());
+                toBest.setFullYear(to.getFullYear());
+              }
+              streak = 0;
+              isToSet = 0;
+              ++i;
+              dat.setDate(dat.getDate() - 1);
+            }
+          }
+          _this.longestStreak(bestStreak + " days");
+          _this.longestStreakPeriod(getPeriodString(fromBest, toBest));
         } else {
-          _this.currentStreak(streak + " days");
-          _this.currentStreakPeriod(getPeriodString(from, to));
+          _this.currentStreak("0 days");
+          _this.currentStreakPeriod("No recent contributions");
+          _this.longestStreak("0 days");
+          _this.longestStreakPeriod("No recent contributions");
         }
-
-
-        var dat = new Date();
-        dat.setDate(dat.getDate() - 1);
-        var from = new Date();
-        from.setDate(from.getDate() - 1);
-        var to = new Date();
-        to.setDate(to.getDate() - 1);
-        var fromBest = new Date();
-        fromBest.setDate(fromBest.getDate() - 1);
-        var toBest = new Date();
-        toBest.setDate(toBest.getDate() - 1);
-
-        var streak = 0;
-        var bestStreak = 0;
-        var isToSet = 0;
-
-        for (var i = uniqueDates.length - 1; i >= 0; i--) {
-          if (dat.getFullYear() == uniqueDates[i].getFullYear()
-            && dat.getMonth() == uniqueDates[i].getMonth()
-            && dat.getDay() == uniqueDates[i].getDay()) {
-            streak++;
-            if (isToSet === 0) {
-              to.setDate(dat.getDate());
-              to.setMonth(dat.getMonth());
-              to.setFullYear(dat.getFullYear());
-              isToSet = 1;
-            }
-            dat.setDate(dat.getDate() - 1);
-          } else {
-            from.setDate(dat.getDate() + 1);
-            from.setMonth(dat.getMonth());
-            from.setFullYear(dat.getFullYear());
-
-            if (streak > bestStreak) {
-              bestStreak = streak;
-              fromBest.setDate(from.getDate());
-              fromBest.setMonth(from.getMonth());
-              fromBest.setFullYear(from.getFullYear());
-              toBest.setDate(to.getDate());
-              toBest.setMonth(to.getMonth());
-              toBest.setFullYear(to.getFullYear());
-            }
-            streak = 0;
-            isToSet = 0;
-            ++i;
-            dat.setDate(dat.getDate() - 1);
-          }
-        }
-        _this.longestStreak(bestStreak + " days");
-        _this.longestStreakPeriod(getPeriodString(fromBest, toBest));
       });
     };
   }
--- CommitLab.Web/Scripts/ViewModels/CommitsViewModel.js
+++ CommitLab.Web/Scripts/ViewModels/CommitsViewModel.js
@@ -5,7 +5,8 @@
     this.noActivity = ko.observable('');
     this.periodInfo = ko.observable("Period: 1 week");
     this.commitsPeriodTable = ko.observableArray([]);
-    this.information = ko.observableArray([]);
+    this.groupArray = ko.observableArray([]);
+    this.reposArray = ko.observableArray([]);
     var username;
 
     this.setPeriod = function (value) {
@@ -58,7 +59,6 @@
       return getShortMonth(value.getMonth()) + " " + value.getDate();
     }
 
-
     function createInfoString() {
       if (_this.commitsPeriodTable().length === 0) {
         _this.noActivity(username + " has no activity during this period.");
@@ -66,50 +66,95 @@
         for (var i = 0; i < _this.commitsPeriodTable().length; i++) {
           var shortDate = getShortDate(_this.commitsPeriodTable()[i].date);
           if (_this.commitsPeriodTable()[i].number === 1) {
-            var string = "Pushed " + _this.commitsPeriodTable()[i].number + " commit to " + _this.commitsPeriodTable()[i].repositoryName + ", " + shortDate;
+            var string = "Pushed " + _this.commitsPeriodTable()[i].number + " commit to " + _this.commitsPeriodTable()[i].repositoryName + "/" + _this.commitsPeriodTable()[i].branchName + ", " + shortDate;
           } else {
-            var string = "Pushed " + _this.commitsPeriodTable()[i].number + " commits to " + _this.commitsPeriodTable()[i].repositoryName + ", " + shortDate;
+            var string = "Pushed " + _this.commitsPeriodTable()[i].number + " commits to " + _this.commitsPeriodTable()[i].repositoryName + "/" + _this.commitsPeriodTable()[i].branchName + ", " + shortDate;
           }
-          _this.information.push({ info: string });
+          _this.commitsPeriodTable()[i].information = string;
         }
         _this.noActivity('');
+      }
+    }
+
+    function compareCommits(val1, val2) {
+      if (val1.repositoryName > val2.repositoryName) {
+        return -1;
+      } else if (val1.repositoryName < val2.repositoryName) {
+        return 1;
+      } else {
+        if (val1.date > val2.date) {
+          return 1;
+        } else if (val1.date < val2.date) {
+          return -1;
+        }
+        return 0;
+      }
+    }
+
+    function fillGroupArray() {
+      while (_this.groupArray().length > 0) {
+        _this.groupArray.pop();
+      }
+      while (_this.reposArray().length > 0) {
+        _this.reposArray.pop();
+      }
+      for (var i = 0; i < _this.commitsPeriodTable().length; i++) {
+        var exists = 0;
+        for (var j = 0; j < _this.groupArray().length; j++) {
+          if (_this.commitsPeriodTable()[i].repositoryName === _this.groupArray()[j].name) {
+            _this.groupArray()[j].repos.push({ rep: _this.commitsPeriodTable()[i].information });
+            exists = 1;
+          }
+        }
+        if (exists === 0) {
+          _this.groupArray.push({ name: _this.commitsPeriodTable()[i].repositoryName, repos: [] });
+          _this.groupArray()[_this.groupArray().length - 1].repos.push({rep: _this.commitsPeriodTable()[i].information});
+        }
+      }
+      for (var i = 0; i < _this.groupArray().length; i++) {
+        _this.reposArray.push({ name: _this.groupArray()[i].name, repos: _this.groupArray()[i].repos });
       }
     }
 
     this.getCommits = function () {
       var date = new Date();
       date.setDate(date.getDate() - period);
-      while (_this.information().length > 0) {
-        _this.information.pop();
-      }
+
       while (_this.commitsPeriodTable().length > 0) {
         _this.commitsPeriodTable.pop();
       }
+      
+      
       var index = repoDateMap.length - 1;
-      while (repoDateMap[index].time.getFullYear() > date.getFullYear() || repoDateMap[index].time.getMonth() > date.getMonth() || repoDateMap[index].time.getDate() > date.getDate()) {
-        var exists = 0;
-        for (var i = 0; i < _this.commitsPeriodTable().length; i++) {
-          if (repoDateMap[index].repo === _this.commitsPeriodTable()[i].repositoryName
-            && repoDateMap[index].time.getFullYear() === _this.commitsPeriodTable()[i].date.getFullYear()
-            && repoDateMap[index].time.getMonth() === _this.commitsPeriodTable()[i].date.getMonth()
-            && repoDateMap[index].time.getDate() === _this.commitsPeriodTable()[i].date.getDate()) {
-            _this.commitsPeriodTable()[i].number++;
-            exists = 1;
+      if (index >= 0) {
+        while (repoDateMap[index].time.getFullYear() > date.getFullYear() || repoDateMap[index].time.getMonth() > date.getMonth() || repoDateMap[index].time.getDate() > date.getDate()) {
+          var exists = 0;
+          for (var i = 0; i < _this.commitsPeriodTable().length; i++) {
+            if (repoDateMap[index].repo === _this.commitsPeriodTable()[i].repositoryName
+              && repoDateMap[index].branch === _this.commitsPeriodTable()[i].branchName
+              && repoDateMap[index].time.getFullYear() === _this.commitsPeriodTable()[i].date.getFullYear()
+              && repoDateMap[index].time.getMonth() === _this.commitsPeriodTable()[i].date.getMonth()
+              && repoDateMap[index].time.getDate() === _this.commitsPeriodTable()[i].date.getDate()) {
+              _this.commitsPeriodTable()[i].number++;
+              exists = 1;
+            }
           }
+          if (exists === 0) {
+            _this.commitsPeriodTable.push({ number: 1, date: repoDateMap[index].time, repositoryName: repoDateMap[index].repo, information: "", branchName: repoDateMap[index].branch });
+          }
+          --index;
         }
-        if (exists === 0) {
-          _this.commitsPeriodTable.push({ number: 1, date: repoDateMap[index].time, repositoryName: repoDateMap[index].repo });
-        }
-        --index;
       }
+      _this.commitsPeriodTable.sort(compareCommits);
+
       createInfoString();
+      fillGroupArray();
+
     };
 
     this.getCommitsOneDay = function (value) {
       _this.periodInfo("Period: " + getDate(value));
-      while (_this.information().length > 0) {
-        _this.information.pop();
-      }
+
       while (_this.commitsPeriodTable().length > 0) {
         _this.commitsPeriodTable.pop();
       }
@@ -117,18 +162,20 @@
         if (repoDateMap[i].time.getFullYear() === value.getFullYear() && repoDateMap[i].time.getMonth() === value.getMonth() && repoDateMap[i].time.getDate() === value.getDate()) {
           var exists = 0;
           for (var j = 0; j < _this.commitsPeriodTable().length; j++) {
-            if (repoDateMap[i].repo === _this.commitsPeriodTable()[j].repositoryName) {
+            if (repoDateMap[i].repo === _this.commitsPeriodTable()[j].repositoryName
+              && repoDateMap[i].branch === _this.commitsPeriodTable()[j].branchName) {
               _this.commitsPeriodTable()[j].number++;
               exists = 1;
             }
           }
           if (exists === 0) {
-            _this.commitsPeriodTable.push({ number: 1, date: repoDateMap[i].time, repositoryName: repoDateMap[i].repo });
+            _this.commitsPeriodTable.push({ number: 1, date: repoDateMap[i].time, repositoryName: repoDateMap[i].repo, information: "", branchName: repoDateMap[i].branch });
           }
         }
       }
       
       createInfoString();
+      fillGroupArray();
     };
 
   }
