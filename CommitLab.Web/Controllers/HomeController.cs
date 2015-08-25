1aa2f7786c396b502d51ee58c0f6460aa103ceb1
azuraw10 <azuraw10@gmail.com>
2015-08-25 09:45:51 +0200

    no msg

--- CommitLab.Web/Controllers/HomeController.cs
+++ CommitLab.Web/Controllers/HomeController.cs
@@ -6,26 +6,21 @@ namespace CommitLab.Web.Controllers
   public class HomeController : Controller
   {
     public ActionResult Index(string id)
-    {     
-      if (string.IsNullOrWhiteSpace(id))
-      {
-        return View((object)ParseLoginName(User.Identity.Name));
-      }
-      else
-      {
-        return View((object)id);
-      }
-    }
-
-    private string ParseLoginName(string model)
     {
-      var match = Regex.Match(model, @"KI-CENTRALA\\([A-Za-z0-9.]+)");
-      return match.Groups[1].Value;
+      var model = string.IsNullOrWhiteSpace(id) ? ParseLoginName(User.Identity.Name) : id;
+
+      return View((object)model);
     }
 
     public ActionResult Search()
     {
       return View();
+    }
+
+    private string ParseLoginName(string model)
+    {
+      var match = Regex.Match(model, @"([A-Za-z0-9.-]+\\)?([A-Za-z0-9.]+)");
+      return match.Groups[1].Value;
     }
   }
 }
